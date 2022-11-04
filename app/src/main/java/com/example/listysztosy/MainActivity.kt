c7317747edf346b68ce3b9765b90e3cd6bd9e994
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-04 15:18:12 +0100

    dodawnaie stosu

--- app/src/main/java/com/example/listysztosy/MainActivity.kt
+++ app/src/main/java/com/example/listysztosy/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.listysztosy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +13,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val stos = LinkedList<Int>()
-        val dodaj = findViewById<Button>(R.id.button)
-        val usun = findViewById<Button>(R.id.button2)
-    }
+        val dodajstos = findViewById<Button>(R.id.button)
+        val usunstos = findViewById<Button>(R.id.button2)
+        val napis = findViewById<EditText>(R.id.editTextTextPersonName)
+        val text = findViewById<TextView>(R.id.textView)
+        dodajstos.setOnClickListener {
+            if (stos.isEmpty()) {
+                usunstos.isEnabled = true
+                stos.addFirst(napis.text.toString().toInt())
+                text.text = stos.first.toString()
+            }
+            else {
+                stos.add(dodajstos.text.toString().toInt())
+                text.text = stos.last.toString()
+            }
+        }
 
+    }
 }
