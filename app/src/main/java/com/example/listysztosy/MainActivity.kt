1de3c5f83c4720a5da721f16945cbd39506e2fc8
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-04 16:52:47 +0100

    stos jest pusty wyswietanie

--- app/src/main/java/com/example/listysztosy/MainActivity.kt
+++ app/src/main/java/com/example/listysztosy/MainActivity.kt
@@ -18,11 +18,9 @@ class MainActivity : AppCompatActivity() {
         val stos = LinkedList<Int>()
         val dodajstos = findViewById<Button>(R.id.button2)
         val usunstos = findViewById<Button>(R.id.button)
-        val pokazpierwszy = findViewById<Button>(R.id.button3)
         val pokazostatni = findViewById<Button>(R.id.button4)
         val napis = findViewById<EditText>(R.id.editTextNumber)
         val ostatni = findViewById<TextView>(R.id.textView)
-        val pierwszy = findViewById<TextView>(R.id.textView2)
         dodajstos.setOnClickListener {
             if (stos.isEmpty()) {
                 stos.addFirst(napis.text.toString().toInt())
@@ -36,12 +34,9 @@ class MainActivity : AppCompatActivity() {
             stos.removeLast()
             if (stos.isEmpty())
             {
-                usunstos.isEnabled = false
+                ostatni.text = "stos jest pusty!"
             }
             }
-        pokazpierwszy.setOnClickListener {
-            pierwszy.text = stos.first.toString()
-        }
         pokazostatni.setOnClickListener {
             ostatni.text = stos.last.toString()
         }
@@ -50,11 +45,24 @@ class MainActivity : AppCompatActivity() {
          val dodajkolejka = findViewById<Button>(R.id.button5)
         val usunkolejka = findViewById<Button>(R.id.button6)
         val textkolejka = findViewById<EditText>(R.id.editTextNumber2)
-        val pokaz = findViewById<Button>(R.id.button7)
+        val pokazost = findViewById<Button>(R.id.button7)
+        val pokazpierw = findViewById<Button>(R.id.button8)
+        val ostatnikolejka = findViewById<TextView>(R.id.textView4)
+        val pierwszykolejka = findViewById<TextView>(R.id.textView5)
 
         dodajkolejka.setOnClickListener {
             kolejka.add(textkolejka.text.toString().toInt())
         }
-
+        usunkolejka.setOnClickListener {
+            kolejka.remove()
+        }
+        pokazost.setOnClickListener {
+            val ost = kolejka.last()
+            ostatnikolejka.text = ost.toString()
+        }
+        pokazpierw.setOnClickListener {
+            val pierw = kolejka.first()
+            pierwszykolejka.text = pierw.toString()
+        }
     }
 }
