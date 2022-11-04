4cdcc6bba7b20809297c1d32e76b34b46eea8a33
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-04 15:55:35 +0100

    dodawanie i usuwanie stosu i przyciski pokazujace

--- app/src/main/java/com/example/listysztosy/MainActivity.kt
+++ app/src/main/java/com/example/listysztosy/MainActivity.kt
@@ -1,33 +1,49 @@
 package com.example.listysztosy
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val stos = LinkedList<Int>()
-        val dodajstos = findViewById<Button>(R.id.button)
-        val usunstos = findViewById<Button>(R.id.button2)
-        val napis = findViewById<EditText>(R.id.editTextTextPersonName)
-        val text = findViewById<TextView>(R.id.textView)
+        val dodajstos = findViewById<Button>(R.id.button2)
+        val usunstos = findViewById<Button>(R.id.button)
+        val pokazpierwszy = findViewById<Button>(R.id.button3)
+        val pokazostatni = findViewById<Button>(R.id.button4)
+        val napis = findViewById<EditText>(R.id.editTextNumber)
+        val ostatni = findViewById<TextView>(R.id.textView)
+        val pierwszy = findViewById<TextView>(R.id.textView2)
         dodajstos.setOnClickListener {
             if (stos.isEmpty()) {
-                usunstos.isEnabled = true
                 stos.addFirst(napis.text.toString().toInt())
-                text.text = stos.first.toString()
+                usunstos.isEnabled = true
             }
             else {
-                stos.add(dodajstos.text.toString().toInt())
-                text.text = stos.last.toString()
+                stos.add(napis.text.toString().toInt())
             }
         }
-
+        usunstos.setOnClickListener {
+            stos.removeLast()
+            if (stos.isEmpty())
+            {
+                usunstos.isEnabled = false
+            }
+            }
+        pokazpierwszy.setOnClickListener {
+            pierwszy.text = stos.first.toString()
+        }
+        pokazostatni.setOnClickListener {
+            ostatni.text = stos.last.toString()
+        }
     }
 }
